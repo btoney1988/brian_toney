b6d81bb6d660e6de172ce0557d98d743f39462d0
Brian Toney <btoney1988@gmail.com>
2020-09-06 22:10:15 -0400

    adding to portfolio with github links in contact and under each project

---
+++ script.js
@@ -0,0 +1,6 @@
+$("#pass-gen").click(function () {
+  window.location.href = "https://btoney1988.github.io/password_generator/";
+});
+$("#code-quiz").click(function () {
+  window.location.href = "https://btoney1988.github.io/coding_quiz/";
+});
